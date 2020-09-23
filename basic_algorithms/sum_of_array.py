59a9498772ab1e6288d7add37721bd25c52529d2
Alena <alenaryzhko1@gmail.com>
2020-09-23 11:07:13 -0400

    sum_of_array in basic folder

---
+++ basic_algorithms/sum_of_array.py
@@ -0,0 +1,31 @@
+"""
+Given an array of integers, find sum of its elements.
+"""
+
+
+# Python program for sum of the array elements
+
+# functions to find sum of elements
+
+# Approach basic_algorithms
+def sum_1(arr):
+    result = 0
+    for x in arr:
+        result += x
+    return result
+
+
+# Approach 2            return sum using sum() function
+def sum_2(arr):
+    result = sum(arr)
+    return result
+
+
+# main function
+if __name__ == "__main__":
+    arr = [10, 20, 30, 40, 50]
+    print('sum_1: {}'.format(sum_1(arr)))
+    print('sum_2: {}'.format(sum_2(arr)))
+    assert (sum_1(arr)) == 150
+
+
