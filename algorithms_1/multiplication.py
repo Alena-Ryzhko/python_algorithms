839575ec2d53c5d3e6e53e33a770aaa9243cb61f
Alena <alenaryzhko1@gmail.com>
2020-09-24 12:32:53 -0400

    added multiplication.py to algorithms_1

---
+++ algorithms_1/multiplication.py
@@ -0,0 +1,16 @@
+"""
+Write a program which finds multiplication of two numbers.
+"""
+
+
+def multiplication(number_1, number_2):
+
+    iterator = 0
+    result = 0
+    while iterator < number_2:
+          result += number_1
+          iterator += 1
+    return result
+
+
+print(multiplication(6786, 776))
