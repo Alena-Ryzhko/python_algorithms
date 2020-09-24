696b2277fb2980e593768e931ed2f928d16d768d
Alena <alenaryzhko1@gmail.com>
2020-09-24 12:56:30 -0400

    added anagram_check.py to algorithms_1

---
+++ algorithms_1/anagram_check.py
@@ -0,0 +1,32 @@
+"""
+Anagram Check.
+
+Given two strings str1 and str2, check if both the strings are anagrams of each other.
+Input : str1 = "listen"
+        str2 = "silent"
+Output :  True                       # (because The strings are anagrams.)
+
+
+Input : str1 = "dad"
+        str2 = "bad"
+Output : False                       # (because The strings aren't anagrams.)
+"""
+
+
+str1 = input(f"Enter First word ")
+str2 = input(f"Enter Second word ")
+
+
+def anagram_check(str1, str2):
+
+    str1 = str1.upper()
+    str2 = str2.upper()
+    if str1 == str2 or len(str1) != len(str2):
+        return False
+    for char in str1:
+        if str1.count(char) != str2.count(char):
+            return False
+    return True
+
+
+print(anagram_check(str1, str2))
