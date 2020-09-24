f481b4642bec8fc6bc4915e7b07aa99a1a47abbd
Alena <alenaryzhko1@gmail.com>
2020-09-23 22:05:09 -0400

    created new file  count_even_odd_digits_of_number

---
+++ basic_algorithms/count_even_odd_digits_of_number.py
@@ -0,0 +1,24 @@
+"""
+Count Even/Odd Digits of Number
+"""
+# D_2
+
+n = int(input(f'Enter a number '))
+
+
+def count_odd_even(n):
+
+    odd_count = 0
+    even_count = 0
+    while n > 0:
+        digit = n % 10
+        if digit % 2 == 0:
+            even_count += 1
+        else:
+            odd_count += 1
+        n = n // 10
+    print(f"Odd numbers in number  {odd_count}")
+    print(f"Even numbers in number  {even_count}")
+
+
+count_odd_even(n)
