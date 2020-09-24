97260acf2a90d60c3794d17123d81373c7812ad7
Alena <alenaryzhko1@gmail.com>
2020-09-24 11:01:14 -0400

    added sum_of_natural_numbers_up_to_num.py file to algorithms_1

---
+++ algorithms_2/sum_of_natural_numbers_up_to_num.py
@@ -0,0 +1,17 @@
+"""
+Write a program to find the sum of n natural numbers using while loop and display it.
+
+"""
+# Sum of natural numbers up to num
+
+num = 16
+
+if num < 0:
+    print("Enter a positive number")
+else:
+    sum = 0
+    # use while loop to iterate until zero
+    while (num > 0):
+        sum += num
+        num -= 1
+    print("The sum is", sum)
