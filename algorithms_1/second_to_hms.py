77887453251a88f0dde7920d734c4001b3fbb725
Alena <alenaryzhko1@gmail.com>
2020-09-24 12:38:00 -0400

    added second_to_hms.py to algorithms_1

---
+++ algorithms_1/second_to_hms.py
@@ -0,0 +1,21 @@
+"""
+Write a function to print Seconds to  Hours, Minutes, Seconds ---> h:m:s
+"""
+
+
+def sec_to_hms(seconds):
+    hours = seconds // 3600
+    minutes = seconds - hours * 3600
+    minutes = seconds // 60
+    seconds = seconds - minutes * 60
+    if hours > 0:
+        return f'{hours}:{minutes}:{seconds}'
+
+    elif minutes > 0:
+        return f'0:{minutes}:{seconds}'
+
+    else:
+        return f'0:0:{seconds}'
+
+
+print(sec_to_hms(3654))
