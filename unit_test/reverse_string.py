ee639d826ef9867950b43880c563852261994951
Alena <alenaryzhko1@gmail.com>
2020-10-09 15:27:11 -0400

    function reverse_string was renamed

--- unit_test/reverse_string.py
+++ unit_test/reverse_string.py
@@ -2,13 +2,13 @@ import unittest
 
 
 class TestStringReversal(unittest.TestCase):
-    def test(self):
+    def test_reverse_string(self):
         input = "Rosa are red and I am glad"
         expected_result = "glad am I and red are Rosa"
-        output = self.reversed_string(input)
+        output = self.reverse_string(input)
         self.assertEqual(expected_result, output)
 
-    def reversed_string(self, input: str):
+    def reverse_string(self, input: str):
         return ' '.join(list(reversed(input.split())))
 
 
