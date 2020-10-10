83993214249a4f763ed4a9cdd41887190dff18f9
Alena <alenaryzhko1@gmail.com>
2020-10-10 16:39:13 -0400

    new function and comments added

--- algoritms_3_str/reverse_string_2.py
+++ algoritms_3_str/reverse_string_2.py
@@ -1,8 +1,24 @@
+"""
+Reverse a string.
+
+A function that takes a String(provided by the user) as an argument
+and returns the backward String:
+"""
+
 string = input(f'Enter the String: ')
 
 
-def reverse_string(string):
+def reverse_string_a(string):
     return ' '.join(list(reversed(string.split())))
 
 
-print(reverse_string(string))
+print(reverse_string_a(string))
+
+
+# or
+
+def reverse_string_b(string):
+    return ' '.join(list(reversed(string.split())))
+
+
+print(reverse_string_b("Rosa is red"))
