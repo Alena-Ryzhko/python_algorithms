d24bbaefe71c3a07fcf27a152e013f1d88d27bbe
“Alena-Ryzhko” <alenaryzhko1@gmail.com>
2024-07-06 20:50:37 -0400

    get rundom number program was updated

---
+++ algorithms_1_num/TEST.py
@@ -0,0 +1,20 @@
+# """
+# A program to get random number:
+# """
+# from random import randint
+# n = randint(100, 999)
+#
+# print(f'We got random number {n}')
+
+
+
+"""
+This propgam to get a rundom number
+"""
+
+from random import randint
+
+n = randint(1, 10)
+
+print(f'we got pundom number {n}')
+
--- algorithms_1_num/get_random_number.py
+++ algorithms_1_num/get_random_number.py
@@ -1,8 +1,11 @@
 """
-A program to get random number:
+A program to generate random numbers.
+A program that generates random numbers for use in various applications.
 """
+
 from random import randint
 n = randint(100, 999)
 
-print(f'We got random number {n}')
+print(f'The generated random number is: {n}')
+print(f'Congratulations! Your random number is: {n}')
 
